d8dd4e2c4fe75bc4861805bbda8e52eaf94e454e
Serban Constantin <serban.constantin@gmail.com>
2016-01-29 16:33:45 +0200

    define initial db schema

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 import configparser
-
 
 description = '''A helper bot which generates stats.'''
 bot = commands.Bot(command_prefix='?', description=description)
@@ -28,6 +31,35 @@ async def messages(name: str, lim: int):
             async for message in bot.logs_from(channel, limit=lim):
                 await bot.say(str(message.timestamp) + ' : ' + message.author.name + ' ' + message.content)
     counter += 1
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    def __repr__(self):
+        return "<User(id='%s', name='%s')>" % (self.id, self.name)
+
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime)
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def __repr__(self):
+        return "<Message(id='%s', timestamp='%s', content='%s', author_id='%s')>" % (
+            self.id, self.timestamp, self.content, self.user_id)
+
+
+engine = create_engine('sqlite:///test.db')
+Base.metadata.create_all(engine)
 
 
 def main():
